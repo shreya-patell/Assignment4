1c5cae7f86e00196bc75dd14f4cf84e272b10243
Shreya Patel <67444978+shreya-patell@users.noreply.github.com>
2021-11-20 18:04:15 -0500

    Add files via upload

---
+++ Exercise1/mavenproject1/src/main/java/com/mycompany/class1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany;
+/**
+ *
+ * @author shreyapatel
+ */
+public class class1 {
+        public static String getAppName() {
+        return "My Company App Ultimate version";
+    
+    }
+}
---
+++ Exercise1/mavenproject2/src/main/java/com/mycompany/class2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany;
+/**
+// *
+ * @author shreyapatel
+ */
+public class class2 {
+    public static void main(String[] args) {
+    String appName = class1.getAppName();
+    System.out.println("Welcome to " + appName);
+    }
+}
